296fb4eed812d740666b4da635a4d2ed33624dfc
teruncius <teruncius@proton.me>
2025-10-16 15:25:40 +0200

    master: installed and configured vitest to run in the browser

---
+++ vitest.config.ts
@@ -0,0 +1,4 @@
+import { mergeConfig } from 'vitest/config';
+import baseConfig from './vite.main.config';
+
+export default mergeConfig(baseConfig, { test: { global: true, browser: { enabled: true, provider: 'playwright', instances: [{ browser: 'chromium' },], }, } });
