7f242e56c6377b12a082e53406c430736d1cbb65
teruncius <teruncius@proton.me>
2025-10-16 22:15:04 +0200

    master: converted ts enum to const with derived type

--- src/app-message.ts
+++ src/app-message.ts
@@ -1,12 +1,14 @@
-export enum AppMessage {
-    ActivateService = "set-active-service",
-    AddService = "add-service",
-    FocusWindow = "focus-application",
-    HideAllServices = "hide-all-services",
-    LoadConfig = "load-config",
-    RemoveService = "remove-service",
-    SaveConfig = "save-config",
-    SetSideBarState = "set-side-bar-state",
-    ShouldUseDarkMode = "should-use-dark-mode",
-    ShowContextMenu = "show-context-menu",
-}
+export const AppMessage = {
+    ActivateService: "set-active-service",
+    AddService: "add-service",
+    FocusWindow: "focus-application",
+    HideAllServices: "hide-all-services",
+    LoadConfig: "load-config",
+    RemoveService: "remove-service",
+    SaveConfig: "save-config",
+    SetSideBarState: "set-side-bar-state",
+    ShouldUseDarkMode: "should-use-dark-mode",
+    ShowContextMenu: "show-context-menu",
+} as const;
+
+export type AppMessage = (typeof AppMessage)[keyof typeof AppMessage];
